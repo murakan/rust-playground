f8e7611fba2f49e0e052111b6e9e088e9ecf20b8
Kan Murata <kan@murakan.info>
2023-08-26 07:49:31 +0900

    [UP] #5 Trait sapmple updated.

--- trait-sample/src/main.rs
+++ trait-sample/src/main.rs
@@ -5,6 +5,9 @@ pub fn _type_of<T>(_: &T) -> String {
     var_type.to_string()
 }
 
+//
+// A sample of implements a generic trait for multiple structs of different types.
+//
 #[derive(Debug)]
 struct Integer {
     val: i32,
@@ -31,14 +34,40 @@ impl ReadData<f32> for Float {
     }
 }
 
-fn show<T: std::fmt::Display, U: ReadData<T>>(obj: U) {
-    println!("{} {}", _type_of(&obj), obj.read());
+fn show<T: std::fmt::Display, U: ReadData<T>>(obj: &U) {
+    println!("{} {}", _type_of(obj), obj.read());
+}
+
+//
+// A sample of the object implement multiple traits which has same method name.
+//
+trait TraitA {
+    fn show(&self);
+}
+
+trait TraitB {
+    fn show(&self);
+}
+
+impl TraitA for Integer {
+    fn show(&self) {
+        println!("TraitA::show called");
+    }
+}
+
+impl TraitB for Integer {
+    fn show(&self) {
+        println!("TraitB::show called");
+    }
 }
 
 fn main() {
     println!("Rust playground!");
     let i = Integer { val: 100 };
     let f = Float { val: 8.2 };
-    show(i);
-    show(f);
+    show(&i);
+    show(&f);
+
+    TraitA::show(&i);
+    TraitB::show(&i);
 }
